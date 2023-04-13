7734aa3b7522f2ed718ffef5e01b5a6fd20c9d2b
kaitoben <3312ben@gmail.com>
2023-04-13 12:46:02 +0900

    コメント投稿機能の変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      @prototype = @comment.prototype
-      @comments = @prototype.comments
-      render "prototypes/show"
+      render :show
     end
   end
 
